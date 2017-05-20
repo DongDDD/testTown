45b0d9781770cfae35d5ba9176ae0859da2f50a4
DongDDD <379247101@qq.com>
2017-05-21 01:32:42 +0800

    sd
    ds

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    // Use this for initialization
+    private Animator ani;
+	void Start () {
+        ani = GetComponent<Animator>();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            ani.SetBool("Attack1", true);
+        }
+
+
+		
+	}
+}
